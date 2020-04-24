8328edbd4a82e505b7cd9071a8c4644a3351b8d7
shashank-vats <shashank.vats8@gmail.com>
2020-04-24 19:45:45 +0530

    Made recyclerview infinite scrollable

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
@@ -2,10 +2,12 @@ package com.example.photogallery;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,11 +20,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PhotoGalleryFragment extends Fragment {
+    private ProgressBar mProgressBar;
     private RecyclerView mPhotoRecyclerView;
+
     private List<GalleryItem> mItems = new ArrayList<>();
     private int mCurrentPage;
+    private boolean mLoading;
 
     private static final String TAG = "PhotoGalleryFragment";
 
@@ -45,17 +51,19 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         mCurrentPage = 1;
+        mLoading = true;
         setUpAdapter();
+        mProgressBar = v.findViewById(R.id.progress_bar);
+        mProgressBar.setIndeterminate(true);
+        mProgressBar.setVisibility(View.GONE);
         mPhotoRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
 
-                if (!mPhotoRecyclerView.canScrollVertically(1) && newState==RecyclerView.SCROLL_STATE_IDLE) {
-                    if (mCurrentPage > 100) {
-                        Toast.makeText(getActivity(), R.string.end_reached_toast, Toast.LENGTH_SHORT).show();
-                    }
-                    Toast.makeText(getActivity(), R.string.loading_toast, Toast.LENGTH_SHORT).show();
+                if (!mPhotoRecyclerView.canScrollVertically(1) && !mLoading && mCurrentPage <= 10) {
+                    mLoading = true;
+                    mProgressBar.setVisibility(View.VISIBLE);
                     new FetchItemsTask().execute();
                 }
             }
@@ -120,14 +128,20 @@ public class PhotoGalleryFragment extends Fragment {
         @Override
         protected List<GalleryItem> doInBackground(Void... params) {
             List<GalleryItem> items = new FlickrFetchr().fetchItems(mCurrentPage);
-            mCurrentPage++;
             return items;
         }
 
         @Override
         protected void onPostExecute(List<GalleryItem> galleryItems) {
-            mItems = galleryItems;
+            Parcelable prevState = Objects.requireNonNull(mPhotoRecyclerView.getLayoutManager()).onSaveInstanceState();
+//            int prevPosition = ((GridLayoutManager) Objects.requireNonNull(mPhotoRecyclerView.getLayoutManager())).findFirstVisibleItemPosition();
+            mItems.addAll(galleryItems);
+            mCurrentPage++;
+            mLoading = false;
+            mProgressBar.setVisibility(View.GONE);
             setUpAdapter();
+//            mPhotoRecyclerView.scrollToPosition(prevPosition);
+            mPhotoRecyclerView.getLayoutManager().onRestoreInstanceState(prevState);
         }
     }
 }
