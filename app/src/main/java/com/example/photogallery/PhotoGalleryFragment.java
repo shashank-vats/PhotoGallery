7bb649eb2ef7fd2906ba0914338495772cc7fcd8
shashank-vats <shashank.vats8@gmail.com>
2020-05-03 07:46:41 +0530

    Added swiperefreshlayout in photogalleryfragment

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
@@ -27,6 +27,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,7 @@ public class PhotoGalleryFragment extends VisibleFragment {
     private ProgressBar mProgressBar;
     private RecyclerView mPhotoRecyclerView;
     private ThumbnailDownloader<Integer> mThumbnailDownloader;
+    private SwipeRefreshLayout mRefreshLayout;
 
     private List<GalleryItem> mItems = new ArrayList<>();
     private int mCurrentPage;
@@ -124,6 +126,17 @@ public class PhotoGalleryFragment extends VisibleFragment {
                         mThumbnailDownloader.queueThumbnail(i, mItems.get(i).getUrl());
                     }
                 }
+            }
+        });
+
+        mRefreshLayout = v.findViewById(R.id.swipe_refresh);
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mCurrentPage = 1;
+                mFreshStart = true;
+                updateItems();
+                mRefreshLayout.setRefreshing(false);
             }
         });
         return v;
